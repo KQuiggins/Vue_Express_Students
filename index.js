ba086f37c1381271459b15f196db251b3a07b1dc
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2023-04-22 15:46:19 -0400

    set port number for heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 const mongoose = require('mongoose');
 
@@ -98,11 +99,6 @@ app.post('/addStudentRecord', async (req, res) => {
 });
 
 
-
-  
-
-
-
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
 });
